db067c3ccea22347e9da60285ab60ea58ca80fde
atsumarukun <atys.smz@gmail.com>
2025-03-16 02:33:36 +0900

    create: GracefulShutdownを導入.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,7 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/atsumarukun/holos-account-api/internal/app/api"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8000")
+	api.Serve()
 }
