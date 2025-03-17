7050ef05ef150a8ff67424e91c211281f76eaa48
atsumarukun <atys.smz@gmail.com>
2025-03-17 22:16:50 +0900

    update: StatusCodeにConflictを追加.

--- internal/app/api/pkg/status/code/code.go
+++ internal/app/api/pkg/status/code/code.go
@@ -4,6 +4,7 @@ type StatusCode string
 
 var (
 	BadRequest   StatusCode = "BAD_REQUEST"
+	Conflict     StatusCode = "CONFLICT"
 	Unauthorized StatusCode = "UNAUTHORIZED"
 	Internal     StatusCode = "INTERNAL"
 )
