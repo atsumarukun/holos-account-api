4150dd37ff2ca0cbad5167550b71863f691f4701
atsumarukun <atys.smz@gmail.com>
2025-03-21 00:29:57 +0900

    create: セッションハンドラーを実装.

--- internal/app/api/interface/handler/session.go
+++ internal/app/api/interface/handler/session.go
@@ -1,6 +1,13 @@
 package handler
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/atsumarukun/holos-account-api/internal/app/api/interface/builder"
+	"github.com/atsumarukun/holos-account-api/internal/app/api/interface/handler/pkg/errors"
+	"github.com/atsumarukun/holos-account-api/internal/app/api/interface/schema"
+	"github.com/atsumarukun/holos-account-api/internal/app/api/pkg/status"
 	"github.com/atsumarukun/holos-account-api/internal/app/api/usecase"
 	"github.com/gin-gonic/gin"
 )
@@ -19,4 +26,22 @@ func NewSessionHandler(sessionUC usecase.SessionUsecase) SessionHandler {
 	}
 }
 
-func (h *sessionHandler) Login(c *gin.Context) {}
+func (h *sessionHandler) Login(c *gin.Context) {
+	var req schema.LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println(err)
+		errors.Handle(c, status.ErrBadRequest)
+		return
+	}
+
+	ctx := c.Request.Context()
+
+	session, err := h.sessionUC.Login(ctx, req.AccountName, req.Password)
+	if err != nil {
+		log.Println(err)
+		errors.Handle(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, builder.ToSessionResponse(session))
+}
