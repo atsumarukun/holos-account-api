7d57ba72011a3ccb238129a649894ab20c05efe9
atsumarukun <atys.smz@gmail.com>
2025-03-20 20:26:54 +0900

    create: セッションエンティティのメンバメソッドを実装.

--- internal/app/api/domain/entity/session.go
+++ internal/app/api/domain/entity/session.go
@@ -1,9 +1,11 @@
 package entity
 
 import (
-	"errors"
+	"crypto/rand"
+	"encoding/base64"
 	"time"
 
+	"github.com/atsumarukun/holos-account-api/internal/app/api/pkg/status"
 	"github.com/google/uuid"
 )
 
@@ -15,13 +17,57 @@ type Session struct {
 }
 
 func NewSession(account *Account) (*Session, error) {
-	return nil, errors.New("not implemented")
+	var session Session
+
+	if err := session.generateID(); err != nil {
+		return nil, err
+	}
+	if err := session.setAccount(account); err != nil {
+		return nil, err
+	}
+	if err := session.GenerateToken(); err != nil {
+		return nil, err
+	}
+
+	return &session, nil
 }
 
 func RestoreSession(id uuid.UUID, accountID uuid.UUID, token string, expiresAt time.Time) *Session {
-	return nil
+	return &Session{
+		ID:        id,
+		AccountID: accountID,
+		Token:     token,
+		ExpiresAt: expiresAt,
+	}
 }
 
 func (s *Session) GenerateToken() error {
-	return errors.New("not implemented")
+	buf := make([]byte, 24)
+	if _, err := rand.Read(buf); err != nil {
+		return err
+	}
+	token := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(buf)
+	if 32 < len(token) {
+		return status.ErrInternal
+	}
+	s.Token = token
+	s.ExpiresAt = time.Now().Add(time.Hour * 24 * 7)
+	return nil
+}
+
+func (s *Session) generateID() error {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+	s.ID = id
+	return nil
+}
+
+func (s *Session) setAccount(account *Account) error {
+	if account == nil {
+		return status.ErrInternal
+	}
+	s.AccountID = account.ID
+	return nil
 }
