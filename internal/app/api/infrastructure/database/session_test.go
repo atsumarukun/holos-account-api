b81febb1a518506aad6f93022acb839f0171bae0
atsumarukun <atys.smz@gmail.com>
2025-03-21 00:51:32 +0900

    create: セッションリポジトリのDelete関数にユニットテストコードを作成.

--- internal/app/api/infrastructure/database/session_test.go
+++ internal/app/api/infrastructure/database/session_test.go
@@ -77,3 +77,65 @@ func TestSession_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestSession_Delete(t *testing.T) {
+	session := &entity.Session{
+		AccountID: uuid.New(),
+		Token:     "1Ty1HKTPKTt8xEi-_3HTbWf2SCHOdqOS",
+		ExpiresAt: time.Now().Add(time.Hour * 24 * 7),
+	}
+
+	tests := []struct {
+		name         string
+		inputSession *entity.Session
+		expectError  error
+		setMockDB    func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name:         "success",
+			inputSession: session,
+			expectError:  nil,
+			setMockDB: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM sessions WHERE account_id = ?;`)).
+					WithArgs(session.AccountID).
+					WillReturnResult(sqlmock.NewResult(1, 1)).
+					WillReturnError(nil)
+			},
+		},
+		{
+			name:         "session is nil",
+			inputSession: nil,
+			expectError:  status.ErrInternal,
+			setMockDB:    func(mock sqlmock.Sqlmock) {},
+		},
+
+		{
+			name:         "delete error",
+			inputSession: session,
+			expectError:  sql.ErrConnDone,
+			setMockDB: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM sessions WHERE account_id = ?;`)).
+					WithArgs(session.AccountID).
+					WillReturnResult(nil).
+					WillReturnError(sql.ErrConnDone)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock := mockDatabase.NewMockDatabase(t)
+			defer db.Close()
+
+			tt.setMockDB(mock)
+
+			repo := database.NewDBSessionRepository(db)
+			if err := repo.Delete(t.Context(), tt.inputSession); !errors.Is(err, tt.expectError) {
+				t.Errorf("\nexpect: %v\ngot: %v", tt.expectError, err)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
