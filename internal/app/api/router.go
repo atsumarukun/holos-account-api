74d0f994070370e3f8ff79ed61d1a59f60f378bc
atsumarukun <atys.smz@gmail.com>
2025-03-23 05:00:50 +0900

    create: 認可ルートを作成.

--- internal/app/api/router.go
+++ internal/app/api/router.go
@@ -6,6 +6,7 @@ func registerRouter(r *gin.Engine) {
 	r.GET("/health", healthHdl.Health)
 	r.POST("/login", sessionHdl.Login)
 	r.DELETE("/logout", authenticationMW.Authenticate, sessionHdl.Logout)
+	r.GET("/authorization", authenticationMW.Authenticate, sessionHdl.Authorize)
 
 	accounts := r.Group("accounts")
 	accounts.POST("/", accountHdl.Create)
